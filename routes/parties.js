c3a57cf80506a46100cff7be169ce802e25f7843
UVSQ21917829 <hajar.ait-ihya@uvsq.fr>
2020-04-29 18:01:41 +0200

    test sauv

--- routes/parties.js
+++ routes/parties.js
@@ -12,7 +12,7 @@ var router = express.Router();
 
 router.get('/', function(req, res)  {
   conn=db.getConnexionDb();
-  let sql = "select p.id,j1.username as gagnant,j2.username as perdu,p.score_g, p.score_p from parties p join joueurs j1 on j1.id=p.id_gagnant join joueurs j2 on j2.id=p.id_perdu where p.id_gagnant = ? OR p.id_perdu = ? ";
+  let sql = "select * from parties   ";
   let query = conn.query(sql, [req.session.ID,req.session.ID], (err, results) => {
     console.log('id test parties:',req.session.ID);
     if(err) throw err;
