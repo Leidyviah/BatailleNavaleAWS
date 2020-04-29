bde159cdb731138c225e80b440894ffa42ade98c
UVSQ21917829 <hajar.ait-ihya@uvsq.fr>
2020-04-29 02:40:43 +0200

    codes

--- routes/joueurs.js
+++ routes/joueurs.js
@@ -1,20 +1,25 @@
 var express = require('express');
 var gameServer = require('../server.js').gameServer;
 var io = require('../server.js').io;
-
+var db = require("../jeu/connexion_db.js");
+var ejs = require('ejs');
+var sha256 = require('js-sha256');
+var ent = require('ent');
+var conn=db.getConnexionDb();
 var router = express.Router(); 
 
-router.get('/', function(req, res) {
-	var username = req.session.username;
+router.get('/', function(req, res)  {
 
-	if (username) {
+  let sql = "SELECT * FROM joueurs";
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+    res.render('joueurs',{
+      results: results
+    });
+    res.end();
+  });
 
-        res.redirect('joueurs')
-		
-	}
 
-    else
-	res.redirect('/'); 
 });
 
 module.exports = router;
--- routes/parties.js
+++ routes/parties.js
@@ -1,10 +1,28 @@
 var express = require('express');
 var gameServer = require('../server.js').gameServer;
 var io = require('../server.js').io;
+var db = require("../jeu/connexion_db.js");
+var ejs = require('ejs');
+var sha256 = require('js-sha256');
+var ent = require('ent');
+var conn=db.getConnexionDb();
 
 
 var router = express.Router(); 
 
+
+router.get('/', function(req, res)  {
+  conn=db.getConnexionDb();
+  let sql = "select p.id,j1.username as gagnant,j2.username as perdu,p.score_g, p.score_p from parties p join joueurs j1 on j1.id=p.id_gagnant join joueurs j2 on j2.id=p.id_perdu where p.id_gagnant = ? OR p.id_perdu = ? ";
+  let query = conn.query(sql, [req.session.ID,req.session.ID], (err, results) => {
+    console.log('id test parties:',req.session.ID);
+    if(err) throw err;
+    res.render('parties',{
+      results: results
+    });
+    res.end();
+  });
+});
 
 router.get('/', function(req, res) {
 
