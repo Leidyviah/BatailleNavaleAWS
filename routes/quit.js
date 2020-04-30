25083a3ee06554ad917caa4e5369d756ea3ee094
Glitch (wood-quick-balaur) <none>
2020-04-30 00:09:15 +0000

    👫👔 Checkpoint ./routes/quit.js:11996484/479

--- routes/quit.js
+++ routes/quit.js
@@ -15,13 +15,20 @@ router.get('/', function(req, res) {
   
   //quit le jeu
   if (gameServer.players[username].game) {
-    io.sockets.to(gameServer.players[username].game.name).emit('quit', {});
+    let game = gameServer.players[username].game;
+    let enemy;
+    if(game.player_one.username == req.session.username){
+      enemy = game.player_two.socketId;
+    } else {
+      enemy = game.player_one.socketId;
+    }
+    io.sockets.broadcast.to(enemy).emit('quit');
 
     gameServer.removeGame(gameServer.players[username].game.name);
     gameServer.updateAvailableGames();
 
   }
-	if (!res.session || !res.session.loggedin){
+	if (!req.session.loggedin){
     gameServer.removePlayer(username);
     req.session.destroy();
 	}
