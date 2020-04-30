a9c139ddf72074c0df1a3e8f47c916fdda35df66
Glitch (wood-quick-balaur) <none>
2020-04-30 01:56:14 +0000

    🐟👝 Checkpoint ./routes/quit.js:11996484/990

--- routes/quit.js
+++ routes/quit.js
@@ -10,31 +10,16 @@ var router = express.Router();
 router.get('/', function(req, res) {
 
 	var username = req.session.username;
-  
+  var enemy = null;
   //quit le jeu
   if (gameServer.players[username].game) {
     let game = gameServer.players[username].game;
     
     if(game.gameType == "multi"){
-      if(game.player_one.username == username){
-        io.sockets.sockets[game.player_two.socketId].leave(game.name);
-        if(!io.sockets.sockets[game.player_two.socketId].handshake.session.loggedin){
-          gameServer.removePlayer(game.player_two.username);
-          io.sockets.sockets[game.player_two.socketId].disconnect();
-        }
-        io.sockets.sockets[game.player_two.socketId].
-      } else {
-        io.sockets.sockets[game.player_one.socketId].leave(game.name);
-        if(!io.sockets.sockets[game.player_one.socketId].handshake.session.loggedin){
-          gameServer.removePlayer(game.player_one.username);
-          io.sockets.sockets[game.player_one.socketId].disconnect();
-        }
-        io.
-      }
+      
+      gameServer.removeGame(game.name);
+      gameServer.updateAvailableGames();
     }
-    
-    gameServer.removeGame(game.name);
-    gameServer.updateAvailableGames();
   }
   if(!req.session.loggedin){
     gameServer.removePlayer(username);
